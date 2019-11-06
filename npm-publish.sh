995715457ad5e501349ecb91085c4d1fe9464679
Francesco Rivola <francesco.rivola@gmail.com>
2019-11-06 22:45:13 +0100

    Migrated CI from CodeShip to GitHub Actions

--- npm-publish.sh
+++
@@ -1,4 +0,0 @@
-npm set init.author.name "Francesco Rivola"
-npm set init.author.email "$NPM_AUTHOR_EMAIL"
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-npm publish --access=public
